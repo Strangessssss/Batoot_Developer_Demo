28ee1380b1e42201100cabbc941742da3f25a2ad
tima <tima.salmanov@icloud.com>
2024-05-20 09:41:00 +0400

    bla

---
+++ Batoot_Developer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Batoot_Developer;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
---
+++ Batoot_Developer/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Batoot_Developer;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    
+    private void ProjectListingView_OnLoaded(object sender, RoutedEventArgs e)
+    {
+        Height = 550;
+        Width = 500;
+        MaxHeight = 550;
+        MinHeight = 550;
+        Background = new SolidColorBrush(Colors.LightBlue);
+    }
+}
